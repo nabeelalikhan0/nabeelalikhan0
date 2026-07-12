53e027e798c2e932f580e60ba3b2656c6a87acea
Nabeel Ali Khan <nabeel03103n@gmail.com>
2026-07-12 12:48:23 +0530

    2026-07-12 12:47:21

---
+++ update.py
@@ -0,0 +1,11 @@
+with open("text.txt","a")as f:
+    f.write("Hello World!")
+
+import subprocess
+import datetime
+
+subprocess.run(["git","add","."])
+cdt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+commit = subprocess.run(["git","commit","-m",f"{cdt}"])
+print(commit)
